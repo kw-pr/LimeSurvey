913eed8c1dfc1639b15a0011d00512ef948dc7d1
Philipp Roos <9133572+kw-pr@users.noreply.github.com>
2018-09-06 11:27:50 +0200

    Fix issue: Update AJAX detection in AdminTheme.php See https://bugs.limesurvey.org/view.php?id=14035

--- application/models/AdminTheme.php
+++ application/models/AdminTheme.php
@@ -137,7 +137,7 @@ class AdminTheme extends CFormModel
         // and move the rest to the bootstrap package.
         // NB: registerAllScripts could be replaced by js definition in package. If needed: not a problem to do it
 
-        if (!Yii::app()->request->getQuery('isAjax', false)) {
+        if (!Yii::app()->request->getIsAjaxRequest()) {
             Yii::app()->getClientScript()->registerMetaTag('width=device-width, initial-scale=1.0', 'viewport'); // See: https://github.com/LimeSurvey/LimeSurvey/blob/master/application/extensions/bootstrap/components/TbApi.php#l108-l115
             App()->bootstrap->registerTooltipAndPopover(); // See : https://github.com/LimeSurvey/LimeSurvey/blob/master/application/extensions/bootstrap/components/TbApi.php#l153-l160
             App()->getClientScript()->registerScript('coreuser', '
